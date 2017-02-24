a3958a544d628fa59b416fe27d844169ec7fcb2d
David Charte <fdavidcl@protonmail.com>
2017-02-24 03:55:15 +0100

    Icons and stuff

--- i3/load_dashboard.sh
+++ i3/load_dashboard.sh
@@ -6,7 +6,7 @@
 # 3. tail -n +2 ~/.i3/workspace_dashboard.json.tmp | fgrep -v '// splitv' | fgrep -v '// splith' | sed 's|//||g' > ~/.i3/workspace_dashboard.json
 
 # First we append the saved layout of worspace N to workspace M
-i3-msg "workspace dashboard;"
+i3-msg "workspace $1;"
 sleep 0.25
 i3-msg "focus parent; focus parent; focus parent; kill; append_layout ~/.i3/workspace_dashboard.json"
 
