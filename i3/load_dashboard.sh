6d27cefec4503c0502ba522395d32198479a1390
David Charte <fdavidcl@protonmail.com>
2017-02-24 16:18:06 +0100

    Minor adjustments

--- i3/load_dashboard.sh
+++ i3/load_dashboard.sh
@@ -6,17 +6,21 @@
 # 3. tail -n +2 ~/.i3/workspace_dashboard.json.tmp | fgrep -v '// splitv' | fgrep -v '// splith' | sed 's|//||g' > ~/.i3/workspace_dashboard.json
 
 # First we append the saved layout of worspace N to workspace M
-i3-msg "workspace $1;"
+current=$(i3-msg -t get_workspaces \
+    | jq '.[] | select(.focused==true).name' \
+    | cut -d"\"" -f2)
+[[ $current = $1 ]] || \
+    i3-msg "workspace $1"
 sleep 0.25
 i3-msg "focus parent; focus parent; focus parent; kill; append_layout ~/.i3/workspace_dashboard.json"
 
 # And finally we fill the containers with the programs they had
-(xfce4-terminal -e 'bash -c "cal;read"' &)
+(xfce4-terminal --hide-scrollbar -e 'bash -c "cal;read"' &)
 sleep 0.25
-(xfce4-terminal -e 'bash -c "clockr"' &)
+(xfce4-terminal --hide-scrollbar -e 'bash -c "clockr"' &)
 sleep 0.25
-(xfce4-terminal -e 'bash -c "wttr;read"' &)
+(xfce4-terminal --hide-scrollbar -e 'bash -c "wttr;read"' &)
 sleep 0.25
 (emacs ~/notes.org &)
 sleep 0.25
-(xfce4-terminal -e 'bash -c "neofetch;read"' &)
+(xfce4-terminal --hide-scrollbar -e 'bash -c "neofetch;read"' &)
