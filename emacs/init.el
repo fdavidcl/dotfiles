f7ed411a853589c22239ff1b40051f19f27c436b
David Charte <fdavidcl@protonmail.com>
2017-04-06 21:51:52 +0200

    Transfer most custom emacs settings to org file

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,19 @@
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Package management
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
+(setq package-enable-at-startup nil)
 
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package))
+
+;; /Customize/ stuff. Afraid to edit this
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,15 +25,15 @@
    ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
  '(blink-cursor-mode t)
  '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-mode t nil (cua-base))
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (solarized-dark)))
+ ;; '(cua-global-mark-cursor-color "#2aa198")
+ ;; '(cua-mode nil nil (cua-base))
+ ;; '(cua-normal-cursor-color "#839496")
+ ;; '(cua-overwrite-cursor-color "#b58900")
+ ;; '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (ample-flat)))
  '(custom-safe-themes
    (quote
-    ("b34636117b62837b3c0c149260dfebe12c5dad3d1177a758bb41c4b15259ed7e" "fe1682ca8f7a255cf295e76b0361438a21bb657d8846a05d9904872aa2fb86f2" "1160f5fc215738551fce39a67b2bcf312ed07ef3568d15d53c87baa4fd1f4d4e" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" "938d8c186c4cb9ec4a8d8bc159285e0d0f07bad46edf20aa469a89d0d2a586ea" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "b34636117b62837b3c0c149260dfebe12c5dad3d1177a758bb41c4b15259ed7e" "fe1682ca8f7a255cf295e76b0361438a21bb657d8846a05d9904872aa2fb86f2" "1160f5fc215738551fce39a67b2bcf312ed07ef3568d15d53c87baa4fd1f4d4e" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(fci-rule-color "#073642")
  '(global-hl-line-mode t)
  '(global-linum-mode t)
@@ -57,6 +66,7 @@
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
+ '(magit-diff-use-overlays nil)
  '(markdown-command "pandoc --webtex")
  '(menu-bar-mode nil)
  '(nrepl-message-colors
@@ -71,7 +81,7 @@
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (org-babel-eval-in-repl ox-reveal ob-ipython org-beautify-theme org-bullets org-gnome column-marker haskell-mode haml-mode drag-stuff ess ess-R-data-view ess-R-object-popup ess-smart-equals ess-smart-underscore pandoc-mode yaml-mode ruby-end fuzzy subatomic-theme powerline paper-theme paradox inf-ruby clippy atom-one-dark-theme latex-extra magithub material-theme flymd jekyll-modes markdown-mode markdown-mode+ markdown-preview-mode neotree magic-latex-buffer magit multi-term nyan-mode solarized-theme org)))
+    (use-package which-key smex ergoemacs-mode ergoemacs-status org-capture-pop-frame color-theme color-theme-sanityinc-tomorrow abyss-theme ample-theme fish-mode sass-mode ox-impress-js org-babel-eval-in-repl ox-reveal ob-ipython org-beautify-theme org-bullets org-gnome column-marker haskell-mode haml-mode drag-stuff ess ess-R-data-view ess-R-object-popup ess-smart-equals ess-smart-underscore pandoc-mode yaml-mode ruby-end fuzzy subatomic-theme powerline paper-theme paradox inf-ruby clippy atom-one-dark-theme latex-extra magithub material-theme flymd jekyll-modes markdown-mode markdown-mode+ markdown-preview-mode neotree magic-latex-buffer magit multi-term nyan-mode solarized-theme org)))
  '(paradox-automatically-star nil)
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -102,14 +112,7 @@
      (320 . "#2793ba")
      (340 . "#268fc6")
      (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+ '(vc-annotate-very-old-color nil t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -117,21 +120,5 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "PfEd" :family "Source Code Pro")))))
 
-; Keybindings
-(global-set-key [f8] 'neotree-toggle)
-(global-set-key (kbd "<f5>") 'magit-status)
-(global-set-key [f12] 'ess-eval-region-or-line-and-step)
-(global-set-key (kbd "C-x C-a") 'mark-whole-buffer)
-
-;; Switch buffers with ctrl-tab
-(global-set-key [C-tab] 'other-window)
-(global-set-key [C-iso-lefttab] 
-    (lambda ()
-      (interactive)
-      (other-window -1)))
-
-(require 'iso-transl)
-
- (add-hook 'text-mode-hook 'visual-line-mode)
- (add-hook 'text-mode-hook 'org-bullets-mode)
- (add-hook 'text-mode-hook 'org-indent-mode)
+;; Load rest of configuration from initialize.org
+(org-babel-load-file (expand-file-name "~/.emacs.d/initialize.org"))
