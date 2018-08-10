f3cbf1828be9be4470208916ef39c15a6af53b81
David Charte <fdavidcl@protonmail.com>
2018-08-10 12:01:13 +0200

    Update 2018-08-10 12:01:13 +0200

--- save.rb
+++ save.rb
@@ -2,7 +2,7 @@
 require 'find'
 require 'fileutils'
 
-blacklist = [".git", "README.md", "LICENSE", "install.rb", "save.rb"]
+blacklist = ["./.git", "./README.md", "./LICENSE", "./install.rb", "./save.rb"]
 dotfiles = Find.find('.').select do |e| 
   !File.directory?(e) && (blacklist.index { |f| e.start_with?(f) }.nil?)
 end
